a939de4c26813733448bbef4249910ba12f1ee2f
AfridiSoftwareAce <afridiwara@ymail.com>
2023-08-07 02:34:49 +0530

    initial commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,121 +1,123 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-
-
+const { generateToken } = require('../services/tokenService');
 // Fetch all users with optional filters, pagination, and sorting
-exports.getAllUsers = async (req, res, next) => {
-  try {
-    const pageSize = parseInt(req.query.pageSize) || 10;
-    const page = parseInt(req.query.page) || 1;
-    const sortBy = req.query.sortBy || 'fullName';
-    const order = req.query.order === 'desc' ? -1 : 1;
-    const filterByCity = req.query.city;
+exports.getAllUsers = async(req, res, next) => {
+    try {
+        const pageSize = parseInt(req.query.pageSize) || 10;
+        const page = parseInt(req.query.page) || 1;
+        const sortBy = req.query.sortBy || 'fullName';
+        const order = req.query.order === 'desc' ? -1 : 1;
+        const filterByCity = req.query.city;
 
-    const query = filterByCity ? { city: filterByCity } : {};
+        const query = filterByCity ? { city: filterByCity } : {};
 
-    const users = await User.find(query)
-      .limit(pageSize)
-      .skip((page - 1) * pageSize)
-      .sort([[sortBy, order]]);
-    const totalUsers = await User.countDocuments(query);
+        const users = await User.find(query)
+            .limit(pageSize)
+            .skip((page - 1) * pageSize)
+            .sort([
+                [sortBy, order]
+            ]);
+        const totalUsers = await User.countDocuments(query);
 
-    res.json({ users, totalPages: Math.ceil(totalUsers / pageSize), currentPage: page });
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.getUserById = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+        res.json({ users, totalPages: Math.ceil(totalUsers / pageSize), currentPage: page });
+    } catch (err) {
+        next(err);
     }
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
 };
 
-exports.createUser = async (req, res, next) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).json({ message: 'User created successfully', user });
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.updateUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+exports.getUserById = async(req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        next(err);
     }
-    res.json({ message: 'User updated successfully', user });
-  } catch (err) {
-    next(err);
-  }
 };
 
-exports.deleteUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+exports.createUser = async(req, res, next) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json({ message: 'User created successfully', user });
+    } catch (err) {
+        next(err);
     }
-    res.json({ message: 'User deleted successfully' });
-  } catch (err) {
-    next(err);
-  }
 };
 
-exports.registerUser = async (req, res, next) => {
-  try {
-    const { fullName, age, city, password } = req.body;
-
-    // Check if user already exists
-    let user = await User.findOne({ fullName });
-    if (user) {
-      return res.status(400).json({ error: 'User already exists' });
+exports.updateUser = async(req, res, next) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json({ message: 'User updated successfully', user });
+    } catch (err) {
+        next(err);
     }
+};
 
-    user = new User({
-      fullName,
-      age,
-      city,
-      password,
-    });
+exports.deleteUser = async(req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+        next(err);
+    }
+};
 
-    await user.save();
+exports.registerUser = async(req, res, next) => {
+    try {
+        const { fullName, age, city, password } = req.body;
 
-    res.status(201).json({ message: 'Registration successful. Welcome!', user });
-  } catch (err) {
-    next(err);
-  }
+        // Check if user already exists
+        let user = await User.findOne({ fullName });
+        if (user) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        user = new User({
+            fullName,
+            age,
+            city,
+            password,
+        });
+
+        await user.save();
+
+        res.status(201).json({ message: 'Registration successful. Welcome!', user });
+    } catch (err) {
+        next(err);
+    }
 };
 
 
 // Login a user
-exports.loginUser = async (req, res, next) => {
-  try {
-    const { fullName, password } = req.body;
+exports.loginUser = async(req, res, next) => {
+    try {
+        const { fullName, password } = req.body;
 
-    // Verify credentials
-    const user = await User.findOne({ fullName });
-    if (!user) {
-      return res.status(400).json({ error: 'Invalid Credentials' });
+        // Verify credentials
+        const user = await User.findOne({ fullName });
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid Credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ error: 'Invalid Credentials' });
+        }
+
+        // Respond with success
+        const token = generateToken(user._id);
+        res.json({ message: 'Login successful. Welcome back!', token });
+    } catch (err) {
+        next(err);
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid Credentials' });
-    }
-
-    // Respond with success
-    res.json({ message: 'Login successful. Welcome back!' });
-  } catch (err) {
-    next(err);
-  }
 };
--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const validationMiddleware = require('../middleware/validationMiddleware');
+const { verifyToken } = require('../middleware/isAuthenticate')
 
 router.get('/', userController.getAllUsers);
 router.post('/register', validationMiddleware.validateRegister, userController.registerUser);
 router.post('/login', validationMiddleware.validateLogin, userController.loginUser);
 
-router.get('/:id', userController.getUserById);
-router.post('/',validationMiddleware.validateUser, userController.createUser);
-router.put('/:id', validationMiddleware.validateUser, userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/:id', verifyToken, userController.getUserById);
+router.post('/', validationMiddleware.validateUser, verifyToken, userController.createUser);
+router.put('/:id', validationMiddleware.validateUser, verifyToken, userController.updateUser);
+router.delete('/:id', verifyToken, userController.deleteUser);
 
 module.exports = router;
